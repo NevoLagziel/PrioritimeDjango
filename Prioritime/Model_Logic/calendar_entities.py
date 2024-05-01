9d90a12b07add78c5897ddd59b8c28602bd2c287
Nevo Lagziel <nevolag@gmail.com>
2024-05-01 19:38:13 +0300

    fix gitignore

---
+++ Prioritime/Model_Logic/calendar_entities.py
@@ -0,0 +1,13 @@
+class CalendarItem:
+    def __init__(self, name):
+        #creation date
+        self.description = None
+        self.duration = None
+        self.recurring = None
+        self.sub_task_list = None
+        self.category = None
+        self.tags = None
+        self.reminders_list = None
+        self.location = None
+
+
